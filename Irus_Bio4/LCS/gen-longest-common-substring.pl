309d263cfc8ddde89169c9ca6127bcf32cc0858d
rsBio <dunedimensia@gmail.com>
2015-10-17 23:22:26 +0300

    Treniruotems longest-common-substring

---
+++ Irus_Bio4/LCS/gen-longest-common-substring.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+
+# Trenituotems :) 
+
+use warnings;
+use strict;
+
+my $str_width = 40;
+my $substr_width = 5;
+my $rows = 8;
+
+srand( time ^ $$ );
+
+my @dict = split //, 'ACGT';
+
+sub _gen_line{
+    my ($length) = shift;
+    return join '', map { $dict[ rand(@dict) ] } 1 .. $length;
+}
+
+my $substr = _gen_line( $substr_width );
+
+my @lines;
+
+for my $i (1 .. $rows){
+    my $line = _gen_line( $str_width );
+    $line = lc $line;
+    my $index = rand $str_width - $substr_width;
+    substr $line, $index, $substr_width, $substr;
+    push @lines, $line;
+    $line = uc $line;
+    print $line . "\n";
+}
+
+<>;
+
+print join "\n", "substr: $substr", @lines, '';
