4347bdf1fc75aa6b5a86da1e2cc8c8e2be4506be
rsBio <dunedimensia@gmail.com>
2015-11-25 15:39:44 +0200

    Pabaigiau 'neg_saske_dinam_trikamp.pl': dabar teisingai apeina duobes. Isveda visa lenta su akumuliuotais taskais langeliuose, ir zvaigzdutemis pazymi, pro kur ejo geriausiu marsrutu. Klaustukais pazymimas paskutinis ateitas langelis. Jeigu tai spastai, tai toliau saske nebeejo. Is paskutiniu isvesciu eiluciu sugeneruotas 'outs-neg' failas. Prideta sudetingesniu testu su duobiu apejimais zigzagu.

---
+++ Irus_Bio4/Dinaminis/Saske/gen-outs-neg.command
@@ -0,0 +1,1 @@
+perl neg_saske_dinam_trikamp.pl tests-with-neg/* | perl -ne 'print if /^\[-?\d+\]$/' > outs-neg
--- Irus_Bio4/Dinaminis/Saske/neg_saske_dinam_trikamp.pl
+++ Irus_Bio4/Dinaminis/Saske/neg_saske_dinam_trikamp.pl
@@ -3,83 +3,101 @@
 use warnings;
 use strict;
 
-#my *from;
+my $trap = -1e3;
 
 while(<>){
-    
-    my ($first_line, @remain) = split /\n/;
-    my @next_lines = map { [ map { $_ == -1 ? -1e3 : $_ } split ' '] } @remain;
-    my @from = map [], ();
-    
-    my ($lines, $rows, $st_pos) = split ' ', $first_line;
-    
-    @{ $next_lines[ 0 ] } = map { $_ ? 0 : undef } @{ $next_lines[ 0 ] };
-#%    print map "$_:\n", join '', map "[$_]", 
-#%        map { sprintf "%2s", defined $_ ? $_ : '.' } @{ $next_lines[ 0 ] }; 
-    
-    for my $i (1 .. @next_lines -1){
-        
-        for my $row ( grep { $_ > 0 and $_ <= $rows } 
-                       map { $st_pos + $_ } -$i .. $i
-        ){
-            my @idxs = grep { $_ > 0 and $_ <= $rows }
-                grep { $_ > $st_pos - $i and $_ < $st_pos + $i } 
-                map { $row + $_ } -1 .. 1;
-#%            print "[@idxs]";
-            my $max_prev = (sort {$b <=> $a} grep defined, 
-                @{ $next_lines[ $i -1 ] }[ map $_ - 1, @idxs ])[ 0 ];
-my $k = -1;            
-for ( grep defined, @{ $next_lines[ $i -1 ] }[ map $_ - 1, @idxs ] ){
+		
+	my ($first_line, @remain) = split /\n/;
+	my @next_lines = map { [ map { $_ == -1 ? $trap : $_ } split ' '] } @remain;
+	
+	my ($lines, $rows, $st_pos) = split ' ', $first_line;
+	
+##	@{ $next_lines[ 0 ] } = map { $_ ? 0 : undef } @{ $next_lines[ 0 ] };
+	@{ $next_lines[ 0 ] } = (0) x $rows;
 
-    $max_prev == $_ and last;
-    $k ++;
-}
-print "<$k>";
-            $from[ $i ][ $row -1 ] = $k;
-            $next_lines[ $i ][ $row -1 ] += $max_prev;      
-##            $next_lines[ $i ][ $row -1 ] = 
-##            defined $max_prev ? $next_lines[ $i ][ $row -1 ] + $max_prev : undef;
+#%	print map "$_:\n", join '', map "[$_]", 
+#%		map { sprintf "%5s", defined $_ ? $_ : '.' } @{ $next_lines[ 0 ] }; 
+	
+	my $ended = 0;
+	
+	for my $i (1 .. @next_lines -1){
+#%		print "i: [$i]\n";
+		my $line_ok = 0;
+		
+		for my $row ( grep { $_ > 0 and $_ <= $rows } 
+						map { $st_pos + $_ } -$i .. $i
+		){
+			my @idxs = grep { $_ > 0 and $_ <= $rows }
+				grep { $_ > $st_pos - $i and $_ < $st_pos + $i } 
+				map { $row + $_ } -1 .. 1;
+#%			print "[@idxs]";
+			my @to_sort = grep defined, 
+				@{ $next_lines[ $i -1 ] }[ map $_ - 1, @idxs ];
+			my $max_prev = (sort {$b <=> $a} @to_sort )[ 0 ];
+#%			print "($max_prev)\n";
+			
+			$next_lines[ $i ][ $row -1 ] >= 0 and $line_ok = 1;
+			$next_lines[ $i ][ $row -1 ] += $max_prev;      
+
         }
-	last if not grep $_ >= 0, @{ $next_lines[ $i ] };
-#%        print map "$_\n", join '', map "[$_]", 
-#%            map { sprintf "%2s" , defined $_ ? $_ : '.' } @{ $next_lines[ $i ] };
+		$ended = $i;
+#%		print "ended: $ended\n";
+		last if not $line_ok;
+
+	}
+
+	my $idx;
+	my $final_max;
+
+	for my $i ($ended){
+
+		my @idxs = map { $_ -1 } (grep { $_ > 0 and $_ <= $rows } 
+				map { $st_pos + $_ } -$i .. $i );
+#%		print "< @idxs >\n";
+
+		$final_max = (sort {$b <=> $a} @{ $next_lines[ $i ] }[ @idxs ] )[ 0 ];
+
+		for (@idxs){
+			$final_max == $next_lines[ $i ][ $_ ] and do { $idx = 1 + $_; last} 
+		}
+#%		print "idx: [$idx]\n";
+		$next_lines[ $i ][ $idx -1 ] = join '', map "?$_?", $next_lines[ $i ][ $idx -1 ];
+	}
+
+#%	print "[[$idx]]\n";
+
+	for my $i (reverse 1 .. $ended -1){
+
+		my @idxs = 
+				map { $_ -1 } (grep { $_ > 0 and $_ <= $rows }
+				grep { $_ > $st_pos - $i -1 and $_ < $st_pos + $i }
+				map { $idx + $_ } -1 .. 1 );
+#%		print "< @idxs >\n";
+
+		my $max = (sort {$b <=> $a} @{ $next_lines[ $i ] }[ @idxs ] )[ 0 ];
+
+		for (@idxs){
+			$max == $next_lines[ $i ][ $_ ] and do { $idx = 1 + $_; last} 
+		}
+#%		print "idx: [$idx]\n";
+
+		$next_lines[ $i ][ $idx -1 ] = join '', map "{$_}", $next_lines[ $i ][ $idx -1 ];
+
+#%		print "-- [[@{ $next_lines[ $i ] }]]\n";
+
     }
-    
-    my $max_back = -1e3;
-    print  ( $max_back = (sort {$b <=> $a} grep defined, 
-                @{ $next_lines[ $lines -1 ] })[ 0 ], "\n" );
 
+	$next_lines[ 0 ][ $st_pos -1 ] = 'start';
+	
+	print map "$_\n", join "\n", map { join '', 
+		map { sprintf "[%7s]", $_ } @{ $next_lines[ $_ ]}  
+		} 0 .. $lines -1;
 
+	print $final_max >= 0 ?
+		"Šaškės surinkti taškai: ${final_max}!\n"
+	:
+		"Šaškė įkrito į duobę!! (atliko ėjimų: ${ended})\n";
 
-    my $j = 0;
-#% print "[[@{ $next_lines[ $lines -1 ] }]] [[$j]]\n";
-    for (@{ $next_lines[ $lines -1 ] }){
-        $_ == $max_back and do { $next_lines[ $lines -1 ][ $j ] = '*'; last };
-        $j ++;
-    }
-#% print "[[@{ $next_lines[ $lines -1 ] }]] [[$j]]\n";
-
-    for my $i (reverse 1 .. @next_lines -2){
-        my $max = -1e3;
-        for (grep { $_ >= 0 and $_ < $rows } map $j + $_, -1 .. 1){
-            $max < $next_lines[ $i ][ $_ ] and $max = $next_lines[ $i ][ $_ ]
-        }
-        for (grep { $_ >= 0 and $_ < $rows } map $j + $_, -1 .. 1){
-            $max == $next_lines[ $i ][ $_ ] and do {
-                $next_lines[ $i ][ $_ ] = '*';
-                $j = $_;
-                last
-            }
-        }
-#% print "-- [[@{ $next_lines[ $i ] }]] [[$j]]\n";
-    }
-
-    print map "$_\n", join "\n", map { join '', 
-        map { sprintf "[%5s]", $_ } @{ $next_lines[ $_ ]}  
-        } 1 .. $rows -1;
-
-    print map "$_\n", join "\n", map { join '', 
-        map { sprintf "[%5s]", $_ } @{ $from[ $_ ]}  
-        } 1 .. $rows -1;
+	print map "[$_]\n", $final_max >= 0 ? $final_max : -1 ;
 
 }
